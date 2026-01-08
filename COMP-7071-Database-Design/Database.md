8d678d565a207d7b3ede5005178fe3bb56790ebe
markus-bcit <markusafonso1@gmail.com>
2026-01-08 08:54:29 -0800

    // 2026-01-08 08:54:29

---
+++ COMP-7071-Database-Design/Database.md
@@ -0,0 +1,4 @@
+[D2L](https://learn.bcit.ca/d2l/home/1196128)
+[Outline](https://www.bcit.ca/outlines/20261092301/)
+
+
---
+++ git_sync.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo -n "Enter commit message: "
+read message
+
+git pull origin main
+
+git add .
+
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+git commit -m "$message // $timestamp"
+
+git push origin main
+
+echo -n "Press Enter to exit..."
+read
